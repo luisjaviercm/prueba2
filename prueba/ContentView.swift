960dac7a20b6570322a877a59d3408a49442d2d6
Luis Javier Contreras Mejía <luisjavier_cm@hotmail.com>
2019-10-17 03:40:29 -0600

    Initial Commit

---
+++ prueba/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  prueba
+//
+//  Created by Luis Javier Contreras Mejía on 10/17/19.
+//  Copyright © 2019 Luis Javier Contreras Mejía. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
